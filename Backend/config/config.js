46e6a50e9a0f22dfbf05e01a62cf2b6931ce0fe6
ruchitadineshentoliya <ruchitadineshentoliya@gmail.com>
2021-10-24 16:06:38 -0700

    mongo connection

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     secret: "cmpe273_secret_key",
-    mongoURI:  'mongodb+srv://admin:admin@indeed.hiqmz.mongodb.net/indeed?retryWrites=true&w=majority',
+    mongoURI:  'mongodb+srv://admin:<password>@indeed.hiqmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 };
 
 module.exports = config;
