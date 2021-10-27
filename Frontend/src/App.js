c7496a0bd01b99eaca09df7f2ba67afc9b5d4a72
Komal Joshi <komal.joshi@sjsu.edu>
2021-10-26 17:14:50 -0700

    Reverted corrupteed changes on App.js mpost merge

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,35 +1,21 @@
+import React, { Component } from 'react'
 import './App.css'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import React from 'react'
-import 'react-toastify/dist/ReactToastify.css'
+import Main from './Main'
+import { BrowserRouter } from 'react-router-dom'
 
-import Login from './pages/Login/Login';
-import Employer from './pages/Employer/Employer';
-import PostJob from './pages/Employer/PostJob';
-import JobSeekerLandingPage from './pages/JobSeeker/JobSeeker'
-import EmployerProfile from './pages/Employer/EmployerProfile'
-
-toast.configure()
-
-function Routing() {
-  return (
-    <Switch>
-        <Route exact path="/login" component={Login} />  
-        <Route exact path="/employer" component={Employer} />   
-        <Route exact path="/postJob" component={PostJob} /> 
-        <Route exact path="/employerprofile" component={EmployerProfile} />  
-        <Route exact path="/landingPage" component={JobSeekerLandingPage} />
-    </Switch>
-  )
+//App Component
+class App extends Component {
+  render() {
+    return (
+      //Use Browser Router to route to different pages
+      <BrowserRouter>
+        <div>
+          {/* App Component Has a Child Component called Main*/}
+          <Main />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
-  )
-}
-
+//Export the App component so that it can be used in index.js
 export default App
