32ef5043fdab5e88aeb74cb053a14b6f6198eb65
shivanid22 <shivanisanjay.degloorkar@sjsu.edu>
2021-10-28 20:11:59 -0700

    Uploading Comapny Tabs Page

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -5,8 +5,9 @@ import Home from './common/Home'
 import Signup from './pages/Signup/Signup.js'
 import Employer from './pages/Employer/Employer'
 import PostJob from './pages/Employer/PostJob'
-import JobSeekerLandingPage from './pages/JobSeeker/JobSeeker.js';
-import Header from './pages/Company/Header.js';
+import JobSeekerLandingPage from './pages/JobSeeker/JobSeeker.js'
+import CompanyTabs from './pages/Company/CompanyTabs.js'
+import Header from './pages/Company/Header.js'
 //Create a Main Component
 class Main extends Component {
   render() {
@@ -15,11 +16,12 @@ class Main extends Component {
         {/*Render Different Component based on Route*/}
         <Route path="/login" component={Login} />
         <Route path="/home" component={Home} />
-        <Route path="/header" component={Header}/>
+        <Route path="/header" component={Header} />
         <Route path="/signup" component={Signup} />
         <Route path="/postJob" component={PostJob} />
         <Route path="/employer" component={Employer} />
         <Route path="/landingPage" component={JobSeekerLandingPage} />
+        <Route path="/company" component={CompanyTabs} />
       </div>
     )
   }
