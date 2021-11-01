1404ff5d48a0fc58e8c64b9414a338afef09ae72
Himakshi <vhimakshi24@gmail.com>
2021-11-01 14:36:30 -0700

    Employer Analytics changes

--- Backend/server.js
+++ Backend/server.js
@@ -50,7 +50,7 @@ app.use(require("./routes/findSalary"))
 app.use(require('./routes/addCompanyDetails'))
 app.use(require("./routes/getCompanyDetails"))
 app.use(require("./routes/addCompanyIdToEmployer"))
-
+app.use(require("./routes/employerAnalytics"))
 //Define all the routes
 //app.use('/postNewJob', postNewJob)
 
