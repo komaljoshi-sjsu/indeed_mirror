71ab6ea7451f898adf42380598ae5608da8f9b13
shivanid22 <shivanisanjay.degloorkar@sjsu.edu>
2021-11-03 10:50:00 -0700

    Added rating in form of stars

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -6,16 +6,12 @@ import Signup from './pages/Signup/Signup.js'
 import Employer from './pages/Employer/Employer'
 import PostJob from './pages/Employer/PostJob'
 import JobSeekerLandingPage from './pages/JobSeeker/JobSeeker.js'
-<<<<<<< HEAD
 import CompanyTabs from './pages/Company/CompanyTabs.js'
 import Header from './pages/Company/Header.js'
-=======
 import UploadPhotos from './pages/JobSeeker/UploadPhotos.js'
 import CompanyPhotos from './pages/JobSeeker/CompanyPhotos'
 import AdminPhoto from './pages/Admin/AdminPhoto.js'
-import Header from './pages/Company/Header.js';
 import EmployerProfile from './pages/Employer/EmployerProfile'
->>>>>>> 1a75148efedf3d2ea5b611ae048c156efbfb1567
 //Create a Main Component
 class Main extends Component {
   render() {
@@ -23,24 +19,18 @@ class Main extends Component {
       <div>
         {/*Render Different Component based on Route*/}
         <Route path="/login" component={Login} />
-<<<<<<< HEAD
         <Route path="/home" component={Home} />
         <Route path="/header" component={Header} />
-=======
-        <Route path="/header" component={Header}/>
->>>>>>> 1a75148efedf3d2ea5b611ae048c156efbfb1567
+        <Route path="/header" component={Header} />
         <Route path="/signup" component={Signup} />
         <Route path="/postJob" component={PostJob} />
         <Route path="/employer" component={Employer} />
         <Route path="/employerprofile" component={EmployerProfile} />
         <Route path="/landingPage" component={JobSeekerLandingPage} />
-<<<<<<< HEAD
         <Route path="/company" component={CompanyTabs} />
-=======
         <Route path="/photos" component={UploadPhotos} />
         <Route path="/companyPhotos" component={CompanyPhotos} />
-        <Route path="/adminPhotos" component={AdminPhoto}/>
->>>>>>> 1a75148efedf3d2ea5b611ae048c156efbfb1567
+        <Route path="/adminPhotos" component={AdminPhoto} />
       </div>
     )
   }
