7c8a948740eeb689c83aa108a8a74635099f9cf9
Sherin Thlakulathil Elias <sherintelias@gmail.com>
2021-11-23 01:26:45 -0800

    Kafka changes for all reviews page

--- Backend/routes/allCompanyReviews.js
+++ Backend/routes/allCompanyReviews.js
@@ -8,22 +8,27 @@ const Company = mongoose.model("Company");
 const mysql = require('mysql');
 const http = require('http');
 const url = require('url');
+const kafka = require('../kafka/client');
 
 router.get("/allReviews", (req, res) => {
 
-    Company.find((error, data) => {
-        if (error) {
+    let msg = {};
+    msg.route = "allReviews";
+
+    kafka.make_request("jobseeker", msg, function (err, results) {
+        if (err) {
             res.writeHead(401,{
                 'Content-Type' : 'application/json'
             });
             res.end("Server error. Please try again later!");
         }
-        else if(data.length > 0){
+        else if (results.status === '200'){
             res.writeHead(200,{
                 'Content-Type' : 'application/json'
             });
-            console.log("Review data : ",JSON.stringify(data));
-            res.end(JSON.stringify(data));
+            
+            console.log("Review data : ",JSON.stringify(results.data));
+            res.end(JSON.stringify(results.data));
             
         }else{
             res.writeHead(400,{
@@ -37,55 +42,32 @@ router.get("/allReviews", (req, res) => {
 
 router.post("/searchReview", (req, res) => {
 
-    const companyName = req.body.companyName;
-    const location = req.body.location;
-    console.log(companyName);
-    console.log(location);
-    let companyIds = [];
-    let sql ='';
-    if(companyName !== '' && companyName !== undefined && location != '' && location !== undefined){
-        sql = "SELECT companyId FROM Company WHERE companyName LIKE "+mysql.escape('%'+companyName+'%') +" and headquarters = "+mysql.escape(location) ;
-    }else if(companyName !== '' && companyName !== undefined ){
-        sql = "SELECT companyId FROM Company WHERE companyName LIKE "+mysql.escape('%'+companyName+'%');
-    }else if(location != '' && location !== undefined){
-        sql = "SELECT companyId FROM Company WHERE headquarters = "+mysql.escape(location);
-    }
-    console.log("sql :" +sql);
-    connection.query(sql, (err, results) => {
+    let msg = {};
+    msg.route = "searchReview";
+    msg.body = req.body;
+
+    kafka.make_request("jobseeker", msg, function (err, results) {
         if (err) {
             res.writeHead(401,{
                 'Content-Type' : 'application/json'
             });
             res.end("Server error. Please try again later!");
         }
-        else if(results.length > 0){
-            for(let i=0; i<results.length; i++){
-                companyIds.push(results[i].companyId);
-            }
-            console.log("company Ids: "+companyIds);
+        else if (results.status === '200'){
+            res.writeHead(200,{
+                'Content-Type' : 'application/json'
+            });
+            
+            console.log("Review data : ",JSON.stringify(results.data));
+            res.end(JSON.stringify(results.data));
+            
+        }else{
+            res.writeHead(400,{
+                'Content-Type' : 'application/json'
+            });
+            console.log("No reviews available!");
+            res.end("No reviews available!!");
         }
-        Company.find({ companyId: { $in: companyIds }}, (error, data) => {
-            if (error) {
-                res.writeHead(401,{
-                    'Content-Type' : 'application/json'
-                });
-                res.end("Server error. Please try again later!");
-            }
-            else if(data.length > 0){
-                res.writeHead(200,{
-                    'Content-Type' : 'application/json'
-                });
-                console.log("Review data : ",JSON.stringify(data));
-                res.end(JSON.stringify(data));
-                
-            }else{
-                res.writeHead(200,{
-                    'Content-Type' : 'application/json'
-                });
-                console.log("No reviews available!");
-                res.end("No reviews available!!");
-            }
-        });	
     });
 });
 
