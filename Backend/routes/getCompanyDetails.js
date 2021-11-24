7a47afd735d74083ae4bf83b5e4cb203bc8171be
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-11-23 23:40:48 -0800

    Migrating to kafka- admin dashboard apis

--- Backend/routes/getCompanyDetails.js
+++ Backend/routes/getCompanyDetails.js
@@ -30,52 +30,52 @@ router.get('/getCompanyDetailsPaginated', function (req, res) {
      kafka.make_request("company", msg, function (err, results) {
          if (err) {
              console.log(err);
-             return res.send({...results,err:err});
+             return res.status(400).send({...results,err:err});
          }
          else {
-             console.log("*******")
-             console.log(results)
-             console.log("*******")
              res.status(200).json({companyDtls: results.companyDtls, count:results.count});
              
          }
      });
  });
- 
+// search company details using company name for admin page 
+router.get('/searchAdminCompany', function (req, res) {
+    console.log("searchAdminCompany.....")
+     let msg = {};
+     msg.route = "searchAdminCompany";
+     msg.query = req.query;
+     //msg = req.body;
+     kafka.make_request("company", msg, function (err, results) {
+         if (err) {
+             console.log(err);
+             return res.status(400).send({...results,err:err});
+         }
+         else {
+             res.status(200).json({companyDtls: results.companyDtls});
+             
+         }
+     });
+ });
 
+ router.get('/companyJobStatistics', function (req, res) {
+    console.log("companyJobStatistics.....")
+     let msg = {};
+     msg.route = "companyJobStatistics";
+     msg.query = req.query;
+     //msg = req.body;
+     kafka.make_request("company", msg, function (err, results) {
+         if (err) {
+             console.log(err);
+             return res.status(400).send({...results,err:err});
+         }
+         else {
+             console.log(results)
+             res.status(200).json({hired: results.hired, rejected:results.rejected});
+             
+         }
+     });
+ });
 
-router.get("/searchAdminCompany", function (req, res) {
-    const searchTerm = "%"+req.query.data+"%"
-    const query = "SELECT * FROM Company WHERE companyName LIKE ?";
-    connection.query(query, [searchTerm], (error, rows) => {
-        if (error) {
-            res.status(400).send("Error occured while retrieving company details");
-        } else{
-            return res.status(200).json({ companyDtls: rows});
-          }
-    });
-});
-
-router.get("/companyJobStatistics", function (req, res) {
-    const companyId= Number(req.query.data)
-    const hired = "Hired";
-    const reject = "Rejected"
-    const countQuery = "SELECT COUNT(*) as count, YEAR(appliedDate) as year FROM AppliedJobs WHERE companyId=? AND status=? GROUP BY YEAR(appliedDate)";
-    connection.query(countQuery, [companyId,hired], (error, rows) => {
-        if (error) {
-            console.log(error)
-            res.status(400).send("Error occured while retrieving company details");
-        } else{
-            connection.query(countQuery, [companyId,reject], function (err, rows2) {
-              if (err) {
-                console.log("Error occured while querying"+err);
-                return res.status(400).send("Error occurred while retrieving pending reviews");
-              }
-              return res.status(200).json({ hired: rows, rejected:rows2});
-            })
-          }
-    });
-});
 
 module.exports = router;
 
--- kafka-backend/services/company/index.js
+++ kafka-backend/services/company/index.js
@@ -1,6 +1,9 @@
 "use strict";
 const { getCompanyDetails } = require("./getCompanyDetails");
 const { getCompanyDetailsPaginated } = require("./getCompanyDetailsPaginated");
+const { searchAdminCompany } = require("./searchAdminCompany");
+const { companyJobStatistics } = require("./companyJobStatistics");
+
 function handle_request(msg, callback) {
     switch (msg.route) {
         case "getCompanyDetails":    
@@ -9,8 +12,14 @@ function handle_request(msg, callback) {
         case "getCompanyDetailsPaginated":    
             getCompanyDetailsPaginated(msg,callback); 
             break;
-           
+        case "searchAdminCompany":    
+            searchAdminCompany(msg,callback); 
+            break; 
+        case "companyJobStatistics":    
+            companyJobStatistics(msg,callback); 
+            break;  
     }
+    
 }
 
 exports.handle_request = handle_request;
