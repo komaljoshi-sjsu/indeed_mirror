163cfd761e4fe48dfb00ccd7ac4f5d9363704f23
Sherin Thlakulathil Elias <sherintelias@gmail.com>
2021-11-27 17:51:39 -0800

    validation and kafka changes for add review screen

--- Backend/routes/companyReviews.js
+++ Backend/routes/companyReviews.js
@@ -325,56 +325,23 @@ router.post("/updateHelpfulCount", checkAuth, (req, res) => {
 
 router.post("/saveReview", checkAuth, (req, res) => {
 
-    console.log(req.body);
-    let sql = 'INSERT INTO Review(reviewTitle, reviewerRole, city, state, postedDate, rating, workHappinessScore, learningScore, appraisalScore, reviewComments, pros, cons, ceoApprovalRating, howToPrepare, noHelpfulCount, yesReviewHelpfulCount, isFeatured, adminReviewStatus, jobSeekerId, companyId ) VALUES (?,?,?,?,now(),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ';
-    let data = [req.body.reviewTitle, req.body.reviewerRole, req.body.city, req.body.state, req.body.rating, req.body.workHappinessScore, req.body.learningScore, req.body.appreciationScore, req.body.reviewComments, req.body.pros, req.body.cons, req.body.ceoApprovalRating, req.body.howToPrepare, 0, 0, 0,'PENDING_APPROVAL', req.body.jobSeekerId, req.body.companyId];
-    
-    connection.query(sql, data, (err, results) => {
+    let msg = {};
+    msg.route = "saveReview";
+    msg.body = req.body;
+
+    kafka.make_request("jobseeker", msg, function (err, results) {
         if (err) {
-            console.log(err);
             res.writeHead(401,{
                 'Content-Type' : 'application/json'
             });
             res.end("Server error. Please try again later!");
         }
         else{
-            let sql = 'select round(avg(Rating),1) as avgRating, round(avg(workHappinessScore),1) as avgWHScore, round(avg(learningScore),1) as avgLScore, round(avg(appraisalScore),1) as avgAppScore, round(avg(ceoApprovalRating),1) as avgCeoScore, count(Rating) as totalReviews from Review where companyId=?';
-            connection.query(sql, [req.body.companyId], (err, results) => {
-                if (err) {
-                    console.log(err);
-                }
-                else if(results.length > 0){
-                    console.log('update');
-                    console.log(results[0].avgWHScore);
-                    Company.updateOne({
-                        companyId: req.body.companyId
-                    }, {
-                        $set: {
-                            avgWorkHappinessScore: results[0].avgWHScore,
-                            avgLearningScore: results[0].avgLScore,
-                            avgAppreciationScore: results[0].avgAppScore,
-                            noOfReviews: results[0].totalReviews,
-                            companyAvgRating: results[0].avgRating,
-                            ceoAvgRating: results[0].avgCeoScore
-                        },
-                    },{ upsert: true }, (error, data) => {
-                
-                        if (error) {
-                            console.log(error);
-                        }else{
-                            console.log('true');
-                        }
-                    });
-                }else{
-                    console.log("no data");
-                }
-                console.log('Mongo DB success');
-                res.writeHead(200,{
-                      'Content-Type' : 'application/json'
-                });
-                res.end("Review saved successfully");
-                });       
-            }         
+            res.writeHead(200,{
+                'Content-Type' : 'application/json'
+            });
+            res.end("Review added successfully");
+        }
     });
 });
 
