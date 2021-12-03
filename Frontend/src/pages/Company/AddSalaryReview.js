5aed78533c13c1d9a45fabba7d4c094e60240605
Himakshi <vhimakshi24@gmail.com>
2021-12-03 02:37:17 -0800

    Salary review validation and route

--- Frontend/src/pages/Company/AddSalaryReview.js
+++ Frontend/src/pages/Company/AddSalaryReview.js
@@ -10,7 +10,13 @@ import '../../CSS/FindSalary.css'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@material-ui/core/TextField';
 import {   Row, Col} from 'react-bootstrap';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { Link,useParams  } from 'react-router-dom';
+import {bindActionCreators} from 'redux';
+import {companyActionCreator} from '../../reduxutils/actions';
+import { useHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+
 
 const AddSalaryReview = (props) => {
   const [show, setShow] = useState(false);
@@ -23,7 +29,15 @@ const AddSalaryReview = (props) => {
   const handleClose = () => setShow(false);
   const handleVisible = () => setVisible(true);
   const token = useSelector((state) => state.userInfo.token);
-
+  const revCompId = useSelector((state) => state.company.compid);
+  const revCompName = useSelector((state) => state.company.compName);
+  const dispatch = useDispatch();
+  const setCompId = bindActionCreators(companyActionCreator.setCompId,dispatch);
+  const setCompName = bindActionCreators(companyActionCreator.setCompName,dispatch);
+  let history = useHistory();
+  const [errors,setErrors] = useState("");
+  const [openSuccessModal,setOpenSuccessModal]=useState(false);
+  const handleCloseSuccessModal = () => setOpenSuccessModal(false);
 
   const handleCloseVisible = () => {
     // console.log("func called");
@@ -31,10 +45,11 @@ const AddSalaryReview = (props) => {
     setShow(false);
     setShowEndDate(false);
     setShowOtherBenefits(false);
-    setErrorMessage("")
+    setErrorMessage("");
+    setErrors("");
   }
   const [companyDtls, setCompanyDtls] = useState([]);
-  const [jobDtls, setJobDtls] =useState([]);
+  // const [jobDtls, setJobDtls] =useState([]);
   const [nameComp, setNameComp] =useState(null);
   const [titleJob, setTitleJob] =useState(null);
   const [locJob, setLocJob] =useState(null);
@@ -43,7 +58,12 @@ const AddSalaryReview = (props) => {
   const jobSeekerId = useSelector((state)=>state.userInfo.id);
   const [jobTitleOption, setJobTitleOption] =useState([]);
   const [cityOption, setCityOption] = useState([]);
-  
+
+  const handleCompanyLink = (e) => {
+    setCompId(revCompId);
+    setCompName(revCompName);
+  }
+
   useEffect(() => {
     console.log("companyid",companyId);
     axios.defaults.headers.common["authorization"] = token;
@@ -61,9 +81,7 @@ const AddSalaryReview = (props) => {
       }).catch((err) => {
         console.log(err);
       });
-
-
-      axios.get(`${backendServer}/getCompanyDetails`) .then((response) => {
+      axios.get(`${backendServer}/getCompanyDetails`).then((response) => {
         
         console.log("getCompanyDetails:",response);
         if (response.status === 200) {
@@ -75,37 +93,37 @@ const AddSalaryReview = (props) => {
       });
 
 
-      axios.get(`${backendServer}/jobSeeker/home`) .then((response) => {
+      axios.get(`${backendServer}/jobSeeker/home`).then((response) => {
         console.log(response);
-        if (response.status === 200) {
-          setJobDtls(response.data);
+          // setJobDtls(response.data);
           let jobTitleOptionArray = [];
           let cityOptionArray = [];
-          jobDtls.forEach((obj)=>{
-            console.log("hiknijnjnjm")
+          // console.log("hiknijnjnjm", jobDtls);
+          response.data.forEach((obj)=>{
+            console.log("current job title",obj.jobTitle);
+            console.log("current city",obj.city);
             if(!jobTitleOptionArray.includes(obj.jobTitle))
             {
-              // console.log("options3",obj.jobTitle);
               jobTitleOptionArray.push(obj.jobTitle);
               // console.log("options",jobTitleOption);
             }
             if(!cityOptionArray.includes(obj.city))
             {
-              // console.log("options3",obj.jobTitle);
               cityOptionArray.push(obj.city);
               // console.log("options",jobTitleOption);
             }
-          })
+            console.log("jobTitleOptionArray: ",jobTitleOptionArray);
+            console.log("cityOptionArray: ",cityOptionArray);
+          });
           setJobTitleOption(jobTitleOptionArray);
           setCityOption(cityOptionArray);
-          console.log("JobDtls: ",jobTitleOption);
-      }
+          // console.log("JobDtls: ",jobTitleOption);
       }).catch((err) => {
         console.log(err);
       });
 
 
-  }, []);
+  },[]);
 
 
   const addSalaryReview = (e) => {
@@ -154,13 +172,19 @@ const AddSalaryReview = (props) => {
           console.log( 'jobTitle', titleJob);
             console.log( 'jobLocation',  locJob);
               console.log(' annualPay',  annualPay);
+   
+              
+
+  if (!currentlyWorking || currentlyWorking === '') {
+    setErrors('Please select an option!');
+  }else{
 
     axios.post(`${backendServer}/jobSeeker/postSalaryReview`,{
         currentlyWorking: currentlyWorking,
         companyName:  companyName,
         endDate:  endDate,
-        jobTitle: jobTitle,
-        jobLocation:  jobLocation,
+        jobTitle: titleJob,
+        jobLocation:  locJob,
         annualPay:  annualPay,
         yrsOfExp: yrsOfExp,
         benefits: benefits,
@@ -171,13 +195,16 @@ const AddSalaryReview = (props) => {
           if (response.status === 200) {
             console.log("returned");
             console.log(response.data);
-            window.location = '/addSalaryReview';
+            handleCloseVisible();
+            setOpenSuccessModal(true);
+            // history.push('/addSalaryReview');
           }
         }).catch((err) => {
           console.log("caught an error");
           console.log(err.response);
           setErrorMessage(err.response.data);
         });
+      }
     };
 
   return (
@@ -223,7 +250,8 @@ const AddSalaryReview = (props) => {
           <Card style={{marginTop:'20px'}}>
            <Card.Body>
             <Card.Title>Salary satisfaction</Card.Title>
-            <Card.Text> <a href='/reviews'>Add your rating</a> </Card.Text>
+            {/* <Card.Text> <a href='/reviews'>Add your rating</a> </Card.Text> */}
+            <Link style={{textDecoration: 'none'}} to="/reviews" onClick={(e) => {handleCompanyLink(e) }}>Add your rating</Link>
              </Card.Body>
           </Card>
      </Col>
@@ -257,30 +285,29 @@ const AddSalaryReview = (props) => {
         centered
       >
       <Modal.Header closeButton>
-          <Modal.Title>Can you tell us about yourself?</Modal.Title>
+          <Modal.Title>Can you tell us about yourself?*</Modal.Title>
       </Modal.Header>
       <Modal.Body>
           <Form onSubmit={addSalaryReview}>
               <Form.Group controlId="formFile" className="mb-3">
-                  <Form.Label>What’s your company name?</Form.Label>
+                  <Form.Label>What’s your company name?*</Form.Label>
                     <Autocomplete
                       id="nameComp"
                       options={companyDtls}
                       renderInput={params => (
-                        <TextField {...params} label="Company Name" variant="outlined" style={{dispaly:"flex"}}/>
+                        <TextField {...params} required label="Company Name" variant="outlined" style={{dispaly:"flex"}}/>
                       )}
                       getOptionLabel={option => option.companyName}
                       style={{ width: 270 }}
-                      value={nameComp}
+                      // value={nameComp}
                       name="nameComp"
                       onChange={(_event, companyName) => {
                         setNameComp(companyName);
                       }}
-                      required
                  /><br/>
               </Form.Group>
               <Form.Group>
-                  <Form.Label>Are you currently working at this company?</Form.Label>
+                  <Form.Label>Are you currently working at this company?*</Form.Label>
                   <br/>
                   <ToggleButtonGroup type="radio" name="currentlyWorking">
                     <ToggleButton id="tbg-btn-1" value="Yes" onChange={()=>setShowEndDate(false)}>
@@ -289,63 +316,67 @@ const AddSalaryReview = (props) => {
                     <ToggleButton id="tbg-btn-2" value="No" onChange={()=>setShowEndDate(true)} >
                       No
                     </ToggleButton>
+                    <br/>
+                    <br/>
+                    <Form.Control.Feedback style={{color:"red"}} type="invalid">
+                    {errors}
+                  </Form.Control.Feedback>
                   </ToggleButtonGroup>
+                
                   <br/>
                   <br/>
               </Form.Group>
               { showEndDate ?
               <Form.Group>
                   <Form.Label >End date</Form.Label>
-                  <Form.Control type="text" placeholder="End Date" name="endDate" required/><br/>
+                  <Form.Control type="date" placeholder="End Date" name="endDate" required/><br/>
               </Form.Group> : null
               }
               <Form.Group>
-                  <Form.Label>What’s your job title?</Form.Label>
+                  <Form.Label>What’s your job title?*</Form.Label>
                   <Autocomplete
                       id="jobtitle"
                       // options={jobDtls.filter(companyName => nameComp)}
                       options={jobTitleOption.map((option) => option)}
                       renderInput={params => (
-                        <TextField {...params} label="Job Title" variant="outlined" />
+                        <TextField {...params} required label="Job Title" variant="outlined" />
                       )}
                       getOptionLabel={option => option}
                       style={{ width: 270 }}
-                      value={titleJob}
+                      // value={titleJob}
                       name="jobTitle"
                       onChange={(_event, jobtitle) => {
                         setTitleJob(jobtitle);
                       }}
-                      required
                  /><br/>
               </Form.Group>
               <Form.Group>
-                  <Form.Label>Where’s your job location?</Form.Label>
+                  <Form.Label>Where’s your job location?*</Form.Label>
                   <Autocomplete
                       id="jobloc"
                       options={cityOption.map((option) => option)}
                       renderInput={params => (
-                        <TextField {...params} label="Job Location" variant="outlined" />
+                        <TextField {...params} required label="Job Location" variant="outlined" />
                       )}
                       getOptionLabel={option => option}
                       style={{ width: 270 }}
-                      value={locJob}
+                      // value={locJob}
                       name="jobLocation"
                       onChange={(_event, jobLoc) => {
                         setLocJob(jobLoc);
                       }}
-                      required
                  /><br/>
               </Form.Group>
               <Form.Group>
-                  <Form.Label>What’s your annual pay at the company?</Form.Label>
-                  <Form.Control type="number" placeholder="Annual Pay" name="annualPay" required/><br/>
+                  <Form.Label>What’s your annual pay at the company?*</Form.Label>
+                  <Form.Control type="number" placeholder="Annual Pay" name="annualPay" min="0" required/><br/>
               </Form.Group>
               <Form.Group>
-                  <Form.Label>How many years of relevant experience do you have?</Form.Label>
-                  <Form.Control type="number" placeholder="Experience"  name="yrsOfExp" required/><br/>
+                  <Form.Label>How many years of relevant experience do you have?*</Form.Label>
+                  <Form.Control type="number" placeholder="Experience"  name="yrsOfExp"  min="0"  max="50" required/><br/>
               </Form.Group>
               <Form.Group>
-                  <Form.Label>Which benefits do you receive at the company? </Form.Label> 
+                  <Form.Label>Which benefits do you receive at the company?* </Form.Label> 
                   <Form.Check type="checkbox" label="Paid time off" id="paidTimeOff"/>
                   <Form.Check type="checkbox" label="Health Insurance" id="healthInsurance"/>
                   <Form.Check type="checkbox" label="Life insurance" id="lifeInsurance"/>
@@ -370,10 +401,23 @@ const AddSalaryReview = (props) => {
           </Form>
         </Modal.Body>
       </Modal> 
-
-
+      {openSuccessModal && (
+        <Modal show={openSuccessModal} onHide={handleCloseSuccessModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Success</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Salary Review added successfully!!</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleCloseSuccessModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
   );
 };
 
-export default AddSalaryReview;
+export default withRouter(AddSalaryReview);
